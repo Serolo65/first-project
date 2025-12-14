dec8a0a080c0311f8ff86927d49849111fa22dc4
Serolo65 <123469158+Serolo65@users.noreply.github.com>
2025-12-14 12:38:46 +0200

    Add files via upload

---
+++ enter.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int num1;
+    int num2;
+    int sum;
+    sum=num1 + num2;
+    cout<<"Enter value 1 :"<<endl;
+    cin>>num1;
+    cout<<endl;
+    cout<<"Enter value 2: "<<endl;
+    cin>>num2;
+    cout<<endl;
+    cout<<"sum = "<<sum<<endl;
+    return 0;
+
+}
